ae8cb560196e14665bd4c10926670aa7990534d5
Xuan <108101A1@gm.chihlee.edu.tw>
2023-11-25 13:30:35 +0800

    指定特定站台才能打MemberController的API

--- src/main/java/com/SideProject/ECommerce/controller/FrontEnd/MemberController.java
+++ src/main/java/com/SideProject/ECommerce/controller/FrontEnd/MemberController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.SideProject.ECommerce.vo.ShoppingCartGoodsVo;
 
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/MemberController")
 public class MemberController {
