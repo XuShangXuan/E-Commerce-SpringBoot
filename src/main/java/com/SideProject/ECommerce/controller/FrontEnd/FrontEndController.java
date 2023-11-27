587c24e2237439768d48a999fdbe347672f6280e
Xuan <108101A1@gm.chihlee.edu.tw>
2023-11-27 17:13:37 +0800

    指定特定站台才能打FrontEndController的API

--- src/main/java/com/SideProject/ECommerce/controller/FrontEnd/FrontEndController.java
+++ src/main/java/com/SideProject/ECommerce/controller/FrontEnd/FrontEndController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import com.SideProject.ECommerce.vo.ShoppingCartGoodsVo;
 
 import io.swagger.annotations.ApiOperation;
 
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/FrontEndController")
 public class FrontEndController {
